c256634475b3988e37305b189deb4c219e796332
lifengshuang <b2010000327@gmail.com>
2017-05-22 22:55:11 +0800

    initial commit

--- main.rb
+++ main.rb
@@ -1,8 +1,44 @@
 require 'sinatra'
 require 'json'
+require 'digest'
+require 'fileutils'
+require 'open-uri'
 
-get '/' do
-  halt 400 unless params['name']
-  name = params['name']
-  JSON.dump({'name': "Hello, #{name}"})
+before do
+   headers 'Access-Control-Allow-Origin' => '*', 
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']  
+end
+
+set :public_folder, File.dirname(__FILE__)
+get '/menu' do
+  halt 400 unless params['course']
+  name = params['course']
+  mock = {
+    'project': ['Project 1', 'Project 2'],
+    'lab': []
+  }
+  [200, {'Content-Type' => 'application/json'}, JSON.dump(mock)]
+  # JSON.dump(mock)
+end
+
+get '/file' do
+  send_file File.join(File.dirname(__FILE__), params['name']);
+end
+
+get '/file/gitlab' do
+  halt 400 unless params['url']
+  url = params['url']
+  dir = 'file/gitlab'
+  if (!Dir.exist?(dir))
+    FileUtils.mkdir_p(dir)
+  end
+  file = File.join(dir, Digest::MD5.hexdigest(url))
+  if (File.exist?(file)) 
+    send_file file
+  end
+  data = open(url).read
+  open(file, 'wb') do |f|
+    f << data
+  end
+  send_file file
 end
